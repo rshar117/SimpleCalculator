fad0704bd9e958e43319629fa68e1de1266983af
rshar117 <64862771+rshar117@users.noreply.github.com>
2021-09-29 09:53:19 -0400

    Update MainActivity.java
    Added the correct version

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.simplecalculator;
 
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView equationsText;
+    TextView answerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void clearButton(View view) {
+    }
+    public void minusButton(View view) {
+    }
+    public void plusButton(View view) {
+    }
+    public void multiButton(View view) {
+    }
+    public void divideButton(View view) {
+    }
+    public void nineButton(View view) {
+    }
+    public void eightButton(View view) {
+    }
+    public void sevenButton(View view) {
+    }
+    public void sixButton(View view) {
+    }
+    public void fiveButton(View view) {
+    }
+    public void fourButton(View view) {
+    }
+    public void threeButton(View view) {
+    }
+    public void twoButton(View view) {
+    }
+    public void oneButton(View view) {
+    }
+    public void zeroButton(View view) {
+    }
+    public void equalsButton(View view) {
+    }
+    public void decimalButton(View view) {
+    }
+
 }
